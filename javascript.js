f3e3aefe7864188c48121c55ca7d5b3d004b7dbf
William Ford <wford1.home@gmail.com>
2021-03-04 10:34:52 +0900

    Added all functionality

--- javascript.js
+++ javascript.js
@@ -1,15 +1,32 @@
+const grid = document.querySelector("#grid");
+const reset = document.querySelector("#reset");
+let divBox;
 
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
 
-makeRows(16, 16);
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    for (let i = 0; i < size * size; i++) {
+        let div = document.createElement("div");
+        div.classList.add("divBox");
+        grid.appendChild(div);
+    }
+    divBoxes = document.querySelectorAll(".divBox");
+    divBoxes.forEach((divBox) => {
+        gridBox.addEventListener("mouseover", (e) => {
+            gridBox.classList.add("toggle");
+        });
+    });
+}
+
+
+reset.addEventListener('click', (e) => {
+    divBoxes.forEach((divBox) => {
+        divBox.classList.remove("toggle");
+    });
+});
+
+
+createGrid(16);
